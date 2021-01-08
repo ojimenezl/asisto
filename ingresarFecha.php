7157c4dc36f27eb6b9608d5a435cb97c5a0a83cf
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-08 12:18:42 -0500

    Update ingresarFecha.php

--- ingresarFecha.php
+++ ingresarFecha.php
@@ -40,10 +40,10 @@ function guardarDatos($codigoqr,$req,$connect){
   $BBDDepar=$DeparDato;
   $BBDNaci=$NaciDato;
   $BBDCedula=$CedulaDato;
-  $consulta="INSERT INTO usuario (`nombre`, `usuario`, `cedula`, `correo`, `fecha`) VALUES ('$BBDUsuario','$BBDNombre','$BBDDepar','$BBDCedula','$BBDNaci')";
+  //$consulta="INSERT INTO usuario (`nombre`, `usuario`, `cedula`, `correo`, `fecha`) VALUES ('$BBDUsuario','$BBDNombre','$BBDDepar','$BBDCedula','$BBDNaci')";
     
-  //$consulta="INSERT INTO usuario (usuario) VALUES ('$BBDNombre')";
-  //$consulta="INSERT INTO `usuario` (`nombre`, `usuario`, `cedula`, `correo`, `fecha`, `hora`) VALUES ('9', '9', '9', '9', '9', '2021-01-08 02:00:00')";
+  
+  $consulta="INSERT INTO `usuario` (`nombre`, `usuario`, `cedula`, `correo`, `fecha`, `hora`) VALUES ('9', '9', '9', '9', '9', '2021-01-08 02:00:00')";
   $ejecutar=mysqli_query($connect,$consulta);
 
   if($ejecutar){
