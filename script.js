d9d987e2cde0964958e6c9825a31282d93bcaf03
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-13 10:31:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 $(document).ready(function() {
+    
+    
+function geoloc() {
+  d=document.getElementById("demo");
+  if (navigator.geolocation){
+//     d.innerHTML="<p>Tu dispositivo soporta la geolocalización.</p>";
+    navigator.geolocation.getCurrentPosition(showPosition,showError)
+  }
+else {
+   d.innerHTML="<p>Lo sentimos, tu dispositivo no admite la geolocaización.</p>";
+   }
+}
+function showPosition(position){
+  latitud=position.coords.latitude;
+	longitud=position.coords.longitude;
+  radio=Math.sqrt(15000) * 10;
+	var latd=latitud;
+	var longd=longitud;
+  window.location.href = window.location.href + "?w1=" + latd + "&w2=" + longd;
+}
+function showError(error){
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      d.innerHTML+="<p>El usuario ha denegado el permiso a la localización.</p>"
+      break;
+    case error.POSITION_UNAVAILABLE:
+      d.innerHTML+="<p>La información de la localización no está disponible.</p>"
+      break;
+    case error.TIMEOUT:
+      d.innerHTML+="<p>El tiempo de espera para buscar la localización ha expirado.</p>"
+      break;
+    case error.UNKNOWN_ERROR:
+      d.innerHTML+="<p>Ha ocurrido un error desconocido.</p>"
+      break;
+    }
+  }
 
+
+    
+    
+    
+    
     var qrcode = new QRCode("outputbox");
 
     function makeCode() {
@@ -55,6 +96,7 @@ $(document).ready(function() {
     });
 
     $('#startbtn').click(function() {
+        geoloc();
         $('#startbtn img').hide();
         $('#stopbtn').show();
         var inputF = document.getElementById("msg2");
