28edc7fb09687654572cb0153640b814b0b2ec7e
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-13 11:51:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     
 <script type="text/javascript" src="script.js"></script>
-<script>
+<!-- <script>
 function validar(frm) {
   frm.ingresar.disabled = true;
   for (i=2; i<3; i++)
     if (frm['msg2'].value =='') return
   frm.ingresar.disabled = false;
 }
-</script>
+</script> -->
 </head>
 
     <div id="option">
@@ -58,11 +58,12 @@ function validar(frm) {
                 
                 <video id="preview" class="p-1 border" style="width: 300px;"></video>
              <form  action="ingresarFecha.php" method="post" accept-charset="utf-8">
-                <input type="text" name="msg2" id="msg2" onkeyup = "validar(this.form)" />
+<!--                 <input type="text" name="msg2" id="msg2" onkeyup = "validar(this.form)" /> -->
+		     <input type="text" name="msg2" id="msg2" onkeyup = "validar(this.form)" required="required"/>
 		     <input type="text" name="ubilat" id="ubilat" />
 		     <input type="text" name="ubilon" id="ubilon" />
                 <!--input type="text" name="msg2" id="msg2" size="40" onkeyup="return validar(this.value,'bt1')"-->
-                <input type="submit" id="ingresar" name="ingresar" value="ingresar" disabled="disabled" />
+                <input type="submit" id="ingresar" name="ingresar" value="ingresar" />
              </form>
             </div>
 
