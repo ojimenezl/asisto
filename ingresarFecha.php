ed25348b745910c996f4deec5c71e7ab9ecc542a
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-13 09:17:09 -0500

    Update ingresarFecha.php

--- ingresarFecha.php
+++ ingresarFecha.php
@@ -7,8 +7,37 @@ $txt="userLoadPC.txt";
 if(file_exists($txt)){
 header('Location: http://localhost/codigoQRUser.php');
 }
+if (isset($_GET["w1"])) {
+    // asignar w1 y w2 a dos variables
+    $lond = $_GET["w1"];
+    $latd = $_GET["w2"];
+ 
+    // mostrar $phpVar1 y $phpVar2
+    echo '<script> alert("Ubicacion listo")</script>';
+} else {
+    echo '<script> alert("Ubicacion NO")</script>';
+}
 
-if($connect!=null && $codigoqr!=null){
+ function get_client_ip() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
+$ipuser=get_client_ip();
+if($connect!=null && $codigoqr!=null $$ $ipuser!=null){
   echo'<script> alert("Conectado")</script>';
 
 //   $UsuarioDato=$codigoqr;
@@ -26,7 +55,7 @@ if($connect!=null && $codigoqr!=null){
 //   echo '<script> alert($CedulaDato) </script>';
 
   if($codigoqr!=null){//$NombreDato!=null && $UsuarioDato!=null && $DeparDato!=null && $NaciDato!=null && $CedulaDato!=null){
-   guardarDatos($codigoqr,$req,$connect);//$NombreDato,$UsuarioDato,$DeparDato,$NaciDato,$CedulaDato);
+   guardarDatos($codigoqr,$ipuser,$lond,$latd,$req,$connect);//$NombreDato,$UsuarioDato,$DeparDato,$NaciDato,$CedulaDato);
 
    $fh = fopen($txt, "w") or die("Error al crear");
    $texto=$NombreDato.$UsuarioDato.$DeparDato.$NaciDato.$CedulaDato;
@@ -39,7 +68,7 @@ if($connect!=null && $codigoqr!=null){
 }
 
 
-function guardarDatos($codigoqr,$req,$connect){
+function guardarDatos($codigoqr,$ipuser,$lond,$latd,$req,$connect){
   if(isset($req)){
 
 //   $BBDNombre=$NombreDato;
@@ -47,7 +76,7 @@ function guardarDatos($codigoqr,$req,$connect){
 //   $BBDDepar=$DeparDato;
 //   $BBDNaci=$NaciDato;
 //   $BBDCedula=$CedulaDato;
-  $consulta="INSERT INTO usuario (`nombre`, `usuario`, `cedula`, `correo`, `fecha`) VALUES ('$BBDUsuario','9', '9', '9', '9')";
+  $consulta="INSERT INTO usuario (`nombre`, `usuario`, `cedula`, `correo`, `fecha`) VALUES ('$BBDUsuario','$ipuser', '$lond', '$latd', '9')";
     
   
   //$consulta="INSERT INTO `usuario` (`nombre`, `usuario`, `cedula`, `correo`, `fecha`, `hora`) VALUES ('9', '9', '9', '9', '9', '2021-01-08 02:00:00')";
