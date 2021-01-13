8abc2f667da7d3b199998f15ea0872367eea01fc
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-13 12:54:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ function validar(frm) {
                 <input name="dataNombre" type="text" placeholder="Nombre" id="dataNombre" value="" required="required" />
                 <input name="dataUsuario" type="text" placeholder="Usuario" id="dataUsuario" value="" required="required" />
                 <input name="dataDepar" type="text" placeholder="Departamento" id="dataDepar" value="" required="required" />
-                <input name="dataNaci" type="text" placeholder="Fecha de Nacimiento" id="dataNaci" value="" required="required" />
+                <input name="dataNaci" type="text" placeholder="Fecha " id="dataNaci" value="" required="required" />
                 <input name="dataCedula" type="text" placeholder="Cedula" id="dataCedula" value="" required="required" />
                 <div id="msg">Generaremos tu còdigo QR. Recuerda que es personal.</div>
                 
