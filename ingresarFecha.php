8cd69bc33579dcc71691760a589a70d379070376
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-13 10:08:27 -0500

    Update ingresarFecha.php

--- ingresarFecha.php
+++ ingresarFecha.php
@@ -25,80 +25,80 @@ $txt="userLoadPC.txt";
 
 
 
-//  function get_client_ip() {
-//         $ipaddress = '';
-//         if (getenv('HTTP_CLIENT_IP'))
-//             $ipaddress = getenv('HTTP_CLIENT_IP');
-//         else if(getenv('HTTP_X_FORWARDED_FOR'))
-//             $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-//         else if(getenv('HTTP_X_FORWARDED'))
-//             $ipaddress = getenv('HTTP_X_FORWARDED');
-//         else if(getenv('HTTP_FORWARDED_FOR'))
-//             $ipaddress = getenv('HTTP_FORWARDED_FOR');
-//         else if(getenv('HTTP_FORWARDED'))
-//            $ipaddress = getenv('HTTP_FORWARDED');
-//         else if(getenv('REMOTE_ADDR'))
-//             $ipaddress = getenv('REMOTE_ADDR');
-//         else
-//             $ipaddress = 'UNKNOWN';
-//         return $ipaddress;
-//     }
-// $ipuser=get_client_ip();
+ function get_client_ip() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
+$ipuser=get_client_ip();
 if($connect!=null ){
   echo'<script> alert("Conectado")</script>';
 
-// //   $UsuarioDato=$codigoqr;
-// //   $UsuarioDato=$_POST["dataUsuario"];
-// //   $NombreDato=$_POST["dataNombre"];
-// //   $DeparDato=$_POST["dataDepar"];
-// //   $NaciDato=$_POST["dataNaci"];
-// //   $CedulaDato=$_POST["dataCedula"];
-//    $req=$_REQUEST['ingresar'];
-//   //echo '<script>  alert($codigoqr) </script>';
-//   //echo '<script>  alert($UsuarioDato) </script>';
-// //   echo '<script> alert($NombreDato) </script>';
-// //   echo '<script> alert($DeparDato) </script>';
-// //   echo '<script> alert($NaciDato) </script>';
-// //   echo '<script> alert($CedulaDato) </script>';
+//   $UsuarioDato=$codigoqr;
+//   $UsuarioDato=$_POST["dataUsuario"];
+//   $NombreDato=$_POST["dataNombre"];
+//   $DeparDato=$_POST["dataDepar"];
+//   $NaciDato=$_POST["dataNaci"];
+//   $CedulaDato=$_POST["dataCedula"];
+   $req=$_REQUEST['ingresar'];
+  //echo '<script>  alert($codigoqr) </script>';
+  //echo '<script>  alert($UsuarioDato) </script>';
+//   echo '<script> alert($NombreDato) </script>';
+//   echo '<script> alert($DeparDato) </script>';
+//   echo '<script> alert($NaciDato) </script>';
+//   echo '<script> alert($CedulaDato) </script>';
 
-//   if($codigoqr!=null){//$NombreDato!=null && $UsuarioDato!=null && $DeparDato!=null && $NaciDato!=null && $CedulaDato!=null){
-//    guardarDatos($codigoqr,$ipuser,$req,$connect);//$NombreDato,$UsuarioDato,$DeparDato,$NaciDato,$CedulaDato);
+  if($codigoqr!=null && $ipuser!=null){//$NombreDato!=null && $UsuarioDato!=null && $DeparDato!=null && $NaciDato!=null && $CedulaDato!=null){
+   guardarDatos($codigoqr,$ipuser,$req,$connect);//$NombreDato,$UsuarioDato,$DeparDato,$NaciDato,$CedulaDato);
 
-//    $fh = fopen($txt, "w") or die("Error al crear");
-//    $texto=$NombreDato.$UsuarioDato.$DeparDato.$NaciDato.$CedulaDato;
-//    fwrite($fh, $texto);
-//    fclose($fh);
+   $fh = fopen($txt, "w") or die("Error al crear");
+   $texto=$NombreDato.$UsuarioDato.$DeparDato.$NaciDato.$CedulaDato;
+   fwrite($fh, $texto);
+   fclose($fh);
   }
 
-// }else{
-//   echo'<script> alert("NO Conetado")</script>';
-// }
+}else{
+  echo'<script> alert("NO Conetado")</script>';
+}
 
 
-// function guardarDatos($codigoqr,$ipuser,$req,$connect){
-//   if(isset($req)){
+function guardarDatos($codigoqr,$ipuser,$req,$connect){
+  if(isset($req)){
 
-// //   $BBDNombre=$NombreDato;
-//   $BBDUsuario=$codigoqr;
-// //   $BBDDepar=$DeparDato;
-// //   $BBDNaci=$NaciDato;
-// //   $BBDCedula=$CedulaDato;
-//   $consulta="INSERT INTO usuario (`nombre`, `usuario`, `cedula`, `correo`, `fecha`) VALUES ('$BBDUsuario','$ipuser', '9', '9', '9')";
+//   $BBDNombre=$NombreDato;
+  $BBDUsuario=$codigoqr;
+  $BBDDepar=$ipuser;
+//   $BBDNaci=$NaciDato;
+//   $BBDCedula=$CedulaDato;
+  $consulta="INSERT INTO usuario (`nombre`, `usuario`, `cedula`, `correo`, `fecha`) VALUES ('$BBDUsuario','$BBDDepar', '9', '9', '9')";
     
   
-//   //$consulta="INSERT INTO `usuario` (`nombre`, `usuario`, `cedula`, `correo`, `fecha`, `hora`) VALUES ('9', '9', '9', '9', '9', '2021-01-08 02:00:00')";
-//   $ejecutar=mysqli_query($connect,$consulta);
+  //$consulta="INSERT INTO `usuario` (`nombre`, `usuario`, `cedula`, `correo`, `fecha`, `hora`) VALUES ('9', '9', '9', '9', '9', '2021-01-08 02:00:00')";
+  $ejecutar=mysqli_query($connect,$consulta);
 
-//   if($ejecutar){
-//    echo'<script> alert("Fecha Ingresada")</script>';
+  if($ejecutar){
+   echo'<script> alert("Fecha Ingresada")</script>';
    
-//   }else{
-//     echo'<script> alert("NO Ingresada")</script>';
-//   }
+  }else{
+    echo'<script> alert("NO Ingresada")</script>';
+  }
 
-//   }else{
-//     echo'<script> alert("hola no")</script>';
-//   }
-// }
+  }else{
+    echo'<script> alert("hola no")</script>';
+  }
+}
 
 ?>
