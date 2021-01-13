94974d27fea74d7becf734aec18e67a12aa9e01a
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-13 11:30:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -50,11 +50,11 @@
                 
                 <video id="preview" class="p-1 border" style="width: 300px;"></video>
              <form  action="ingresarFecha.php" method="post" accept-charset="utf-8">
-                <input type="text" name="msg2" id="msg2" />
+                <input type="text" name="msg2" id="msg2" onkeyup = "validar(this.form)" />
 		     <input type="text" name="ubilat" id="ubilat" />
 		     <input type="text" name="ubilon" id="ubilon" />
                 <!--input type="text" name="msg2" id="msg2" size="40" onkeyup="return validar(this.value,'bt1')"-->
-                <input type="submit" id="ingresar" name="ingresar" value="ingresar" />
+                <input type="submit" id="ingresar" name="ingresar" value="ingresar" disabled="disabled" />
              </form>
             </div>
 
