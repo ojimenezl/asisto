7103149336cefa066870f0206bc4c51413e90085
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-13 09:17:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,27 +14,7 @@
         <div id="btn1" class="active">&#8230;</div> 
         <div id="btn2">&#9974;</div>
 
-<?php
-    function get_client_ip() {
-        $ipaddress = '';
-        if (getenv('HTTP_CLIENT_IP'))
-            $ipaddress = getenv('HTTP_CLIENT_IP');
-        else if(getenv('HTTP_X_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-        else if(getenv('HTTP_X_FORWARDED'))
-            $ipaddress = getenv('HTTP_X_FORWARDED');
-        else if(getenv('HTTP_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_FORWARDED_FOR');
-        else if(getenv('HTTP_FORWARDED'))
-           $ipaddress = getenv('HTTP_FORWARDED');
-        else if(getenv('REMOTE_ADDR'))
-            $ipaddress = getenv('REMOTE_ADDR');
-        else
-            $ipaddress = 'UNKNOWN';
-        return $ipaddress;
-    }
-echo get_client_ip();
-?>
+
     </div>
 
     <div id="main">
@@ -91,6 +71,43 @@ echo get_client_ip();
 </body>
 <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
 <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+
+function geoloc() {
+  d=document.getElementById("demo");
+  if (navigator.geolocation){
+    d.innerHTML="<p>Tu dispositivo soporta la geolocalización.</p>";
+    navigator.geolocation.getCurrentPosition(showPosition,showError);
+  }
+else {
+   d.innerHTML="<p>Lo sentimos, tu dispositivo no admite la geolocaización.</p>";
+   }
+}
+function showPosition(position){
+  latitud=position.coords.latitude;
+	longitud=position.coords.longitude;
+  radio=Math.sqrt(15000) * 10;
+	var latd=latitud;
+	var longd=longitud;
+  window.location.href = window.location.href + "?w1=" + latd + "&w2=" + longd;
+}
+function showError(error){
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      d.innerHTML+="<p>El usuario ha denegado el permiso a la localización.</p>"
+      break;
+    case error.POSITION_UNAVAILABLE:
+      d.innerHTML+="<p>La información de la localización no está disponible.</p>"
+      break;
+    case error.TIMEOUT:
+      d.innerHTML+="<p>El tiempo de espera para buscar la localización ha expirado.</p>"
+      break;
+    case error.UNKNOWN_ERROR:
+      d.innerHTML+="<p>Ha ocurrido un error desconocido.</p>"
+      break;
+    }
+  }
+
+
 
 
 </html>
