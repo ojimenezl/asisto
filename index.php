c46f6a1f74258bff204081d11a7ee38ad4d1c51c
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-13 10:27:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -71,43 +71,7 @@
 </body>
 <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
 <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-<script type="text/javascript">
-function geoloc() {
-  d=document.getElementById("demo");
-  if (navigator.geolocation){
-    d.innerHTML="<p>Tu dispositivo soporta la geolocalización.</p>";
-    navigator.geolocation.getCurrentPosition(showPosition,showError);
-  }
-else {
-   d.innerHTML="<p>Lo sentimos, tu dispositivo no admite la geolocaización.</p>";
-   }
-}
-function showPosition(position){
-  latitud=position.coords.latitude;
-	longitud=position.coords.longitude;
-  radio=Math.sqrt(15000) * 10;
-	var latd=latitud;
-	var longd=longitud;
-  window.location.href = window.location.href + "?w1=" + latd + "&w2=" + longd;
-}
-function showError(error){
-  switch(error.code) {
-    case error.PERMISSION_DENIED:
-      d.innerHTML+="<p>El usuario ha denegado el permiso a la localización.</p>"
-      break;
-    case error.POSITION_UNAVAILABLE:
-      d.innerHTML+="<p>La información de la localización no está disponible.</p>"
-      break;
-    case error.TIMEOUT:
-      d.innerHTML+="<p>El tiempo de espera para buscar la localización ha expirado.</p>"
-      break;
-    case error.UNKNOWN_ERROR:
-      d.innerHTML+="<p>Ha ocurrido un error desconocido.</p>"
-      break;
-    }
-  }
-geoloc();
-</script>
+
 
 
 </html>
