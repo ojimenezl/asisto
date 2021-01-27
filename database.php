ddca4a738ce65a01e0abe88e4e3689319db9d982
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-27 12:14:33 -0500

    Update database.php

--- database.php
+++ database.php
@@ -18,24 +18,24 @@
 			if(mysqli_connect_error()){
 				die("Conexión a la base de datos falló " . mysqli_connect_error() . mysqli_connect_errno());
 			}
-		}
-}
+        }
+        
 
-
-public function sanitize($var){
-  $return = mysqli_real_escape_string($this->con, $var);
-  return $return;
-}
-
-
-public function create($nombres,$cedula,$departamento,$ipuser,$latitud,$longitud,$hora){
-    
-	$sql = "INSERT INTO `registro` (`nombre`, `cedula`, `departamento`, `ipuser`, `latitud`, `longitud`, `hora`) VALUES ('$nombres', '$apellidos', '$telefono', '$direccion', '$correo_electronico')";
-	$res = mysqli_query($this->con, $sql);
-	if($res){
-	  return true;
-	}else{
-	return false;
- }
+        public function sanitize($var){
+            $return = mysqli_real_escape_string($this->con, $var);
+            return $return;
+          }
+          
+          
+          public function create($nombres,$cedula,$departamento,$ipuser,$latitud,$longitud,$hora){
+              
+              $sql = "INSERT INTO `registro` (`nombre`, `cedula`, `departamento`, `ipuser`, `latitud`, `longitud`, `hora`) VALUES ('$nombres', '$apellidos', '$telefono', '$direccion', '$correo_electronico')";
+              $res = mysqli_query($this->con, $sql);
+              if($res){
+                return true;
+              }else{
+              return false;
+           }
+          }
 }
 ?>
