eb0c453aab864271b506bc8a9bf9b33a9cf19054
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-29 12:39:06 -0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,10 +5,10 @@ if (isset($_GET['id'])){
 	include('database.php');
 	$cliente = new Database();
 	$id=intval($_GET['id']);
-	echo "foo es $id";
+	
 	$res = $cliente->delete($id);
 	if($res){
-		header('location: https://asisto.herokuapp.com/registros.php');
+		echo "foo es $id";
 	}else{
 		echo "Error al eliminar el registro";
 	}
