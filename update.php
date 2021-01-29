00fc53c72f13a31d82051b490408ccb8b9ff1a3e
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-29 11:24:03 -0500

    Update update.php

--- update.php
+++ update.php
@@ -69,11 +69,12 @@
 				<div class="col-md-6">
 					<label>Nombres:</label>
 					<input type="text" name="nombres" id="nombres" class='form-control' maxlength="100" required value="<?php echo $datos_cliente['nombre'];?>" >
-				</div>
+				        <input type="hidden" name="id" id="id" class='form-control' maxlength="100"   value="<?php echo $datos_cliente->id;?>">
+					</div>
 				<div class="col-md-6">
 					<label>Cédula:</label>
 					<input type="text" name="cedula" id="cedula" class='form-control' maxlength="100" required value="<?php echo $datos_cliente['nombre'];?>">
-                    <!-- <input type="hidden" name="id_cliente" id="id_cliente" class='form-control' maxlength="100"   value="<?php echo $datos_cliente->id;?>"> -->
+
 				</div>
                 <div class="col-md-6">
 					<label>Departamento:</label>
