ec22d8243f41c8a10c49bf2db522f82b1a3b5497
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-01-29 12:33:43 -0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@ if (isset($_GET['id'])){
 	include('database.php');
 	$cliente = new Database();
 	$id=intval($_GET['id']);
-	echo'<script> alert("ID - '.$id.' -  se a guardado con éxito!!")</script>';
+	echo $id;
 	$res = $cliente->delete($id);
 	if($res){
 		header('location: https://asisto.herokuapp.com/registros.php');
