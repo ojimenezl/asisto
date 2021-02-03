198bd1c2562345b8f7725bfd7fada1ca7555fe95
ojimenezl <52500219+ojimenezl@users.noreply.github.com>
2021-02-03 08:51:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
     
-  var aValue = storage.getItem('passuni');
-  if (aValue){
-	  alert("dato storage guardado");
- 
-  }else{
-   var p="12345mm"
-   localStorage.setItem('passuni', p); 
-	  alert("dato NO storage guardado");
-  }
+
 function geoloc() {
   d=document.getElementById("demo");
   if (navigator.geolocation){
@@ -131,6 +123,15 @@ function showError(error){
 	    inputFm.value = "QR Listo!"
                 //$('#msg2').text(content);
         });
+	      var aValue = storage.getItem('passuni');
+  if (aValue){
+	  alert("dato storage guardado");
+ 
+  }else{
+   var p="12345mm"
+   localStorage.setItem('passuni', p); 
+	  alert("dato NO storage guardado");
+  }
 	geoloc();
      let constraints;
     var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
